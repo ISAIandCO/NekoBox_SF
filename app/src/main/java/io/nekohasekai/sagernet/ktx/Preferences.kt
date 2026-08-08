b0d0dbd1ee8d010decf32ce18a6ec53d3ac2ac62
isaico <nikita.gybacheff@gmail.com>
2026-08-08 13:03:36 +0500

    260808 - fasters_regex

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Preferences.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Preferences.kt
@@ -46,7 +46,7 @@ fun PreferenceDataStore.stringToLong(
     name: String,
     defaultValue: () -> Long = { 0L },
 ) = PreferenceProxy(name, defaultValue, { key, default ->
-    getString(key, "$default")?.toLongOrNull() ?: default
+    getString(key, null)?.toLongOrNull() ?: getLong(key, default)
 }, { key, value -> putString(key, "$value") })
 
 class PreferenceProxy<T>(
